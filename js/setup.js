289130b708100a51d35e038f46393d1fe29943e7
d3x4r <dex3r@mail.ru>
2018-11-29 00:34:53 +0500

    Добавлено открытие/закрытие и валидация формы

--- js/setup.js
+++ js/setup.js
@@ -1,53 +1,86 @@
 'use strict';
 
-var removeClass = function (targetClassName, removeClassName) {
-  var targetClass = document.querySelector(targetClassName);
-  targetClass.classList.remove(removeClassName);
+var ESC_KEYCODE = 27;
+var ENTER_KEYCODE = 13;
+
+var formOpen = document.querySelector('.setup-open');
+var form = document.querySelector('.setup');
+var formClose = form.querySelector('.setup-close');
+
+var mageFirstName = [
+  'Иван',
+  'Хуан Себастьян',
+  'Мария',
+  'Кристоф',
+  'Виктор',
+  'Юлия',
+  'Люпита',
+  'Вашингтон'
+];
+
+var mageSecondName = [
+  'да Марья',
+  'Верон',
+  'Мирабелла',
+  'Вальц',
+  'Онопко',
+  'Топольницкая',
+  'Нионго',
+  'Ирвинг'
+];
+
+var coatColor = [
+  'rgb(101, 137, 164)',
+  'rgb(241, 43, 107)',
+  'rgb(146, 100, 161)',
+  'rgb(56, 159, 117)',
+  'rgb(215, 210, 55)',
+  'rgb(0, 0, 0)'
+];
+
+var eyesColor = [
+  'black',
+  'red',
+  'blue',
+  'yellow',
+  'green',
+];
+
+var getOpen = function () {
+  form.classList.remove('hidden');
+
+  document.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === ESC_KEYCODE) {
+      getClose();
+    }
+  });
 };
 
-removeClass('.setup', 'hidden');
-removeClass('.setup-similar', 'hidden');
+var getClose = function () {
+  form.classList.add('hidden');
+};
+
+formOpen.addEventListener('click', function () {
+  getOpen();
+});
+
+formOpen.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    getOpen();
+  }
+});
+
+formClose.addEventListener('click', function () {
+  getClose();
+});
+
+formClose.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    getClose();
+  }
+});
 
 var getRandomMage = function () {
-
-  var mageFirstName = [
-    'Иван',
-    'Хуан Себастьян',
-    'Мария',
-    'Кристоф',
-    'Виктор',
-    'Юлия',
-    'Люпита',
-    'Вашингтон'
-  ];
-
-  var mageSecondName = [
-    'да Марья',
-    'Верон',
-    'Мирабелла',
-    'Вальц',
-    'Онопко',
-    'Топольницкая',
-    'Нионго',
-    'Ирвинг'
-  ];
-
-  var coatColor = [
-    'rgb(101, 137, 164)',
-    'rgb(241, 43, 107)',
-    'rgb(146, 100, 161)',
-    'rgb(56, 159, 117)',
-    'rgb(215, 210, 55)',
-    'rgb(0, 0, 0)'
-  ];
-
-  var eyesColor = [
-    'black',
-    'red',
-    'blue',
-    'yellow',
-    'green',
-  ];
 
   var getMageProperty = function (propertyList) {
     return propertyList[Math.floor(Math.random() * propertyList.length)];
@@ -90,4 +123,17 @@ var similarMageAdd = function (mageCount) {
 similarMageAdd(mageMoc);
 
 similarListElement.appendChild(fragment);
-//1
+
+var getRandomNumber = function (min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+var getRandomProperty = function (propertyArray) {
+  return propertyArray[getRandomNumber(0, propertyArray.length - 1)];
+};
+
+var coat = document.querySelector('.wizard-coat');
+
+coat.addEventListener('click', function () {
+  coat.setAttribute('style', 'fill:' + getRandomProperty(coatColor));
+});
